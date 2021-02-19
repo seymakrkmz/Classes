fb9f20b96a85d54af11c7e92f235ddf2c11b3dbe
Şeyma Korkmaz <72795331+seymakrkmz@users.noreply.github.com>
2021-02-19 22:56:39 +0300

    Classes_2

---
+++ Sepet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Sepet
+    {
+        public void Ekle(Product urun)
+        {
+            Console.WriteLine("Sepete eklendi:" + urun.Adi);
+        }
+        public void Ekle2(string urunAdi, int fiyati)
+        {
+            Console.WriteLine("tebrikler"+ urunAdi);
+        }
+
+    } 
+}
